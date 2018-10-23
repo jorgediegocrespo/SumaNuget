18786a30c85ec7aaf4092a7725300974912381a6
DESKTOP-96VBMK9\Jorge <jorgediegocrespo@gmail.com>
2018-10-23 07:34:10 +0200

    Initial state project

---
+++ SumaNuget/Sumator.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace SumaNuget
+{
+    /// <summary>
+    /// Manage diferent sum operations
+    /// </summary>
+    public class Sumator
+    {
+        /// <summary>
+        /// Sum all the values in a decimal array
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public decimal Suma(decimal[] values)
+        {
+            return values.Sum();
+        }
+
+        /// <summary>
+        /// Sum all the values in an int array
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public int Suma(int[] values)
+        {
+            return values.Sum();
+        }
+    }
+}
